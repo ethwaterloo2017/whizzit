ec573f58ab5cb73274cfb5d6106ad71c5ab5cbea
alexmedkex <alexmedkex@gmail.com>
2017-10-14 22:00:42 +0200

    Added app.js and sheet.js files.

--- truffle/src/js/app.js
+++ truffle/src/js/app.js
@@ -1,68 +1,65 @@
 App = {
-  web3Provider: null,
-  contracts: {},
+    web3Provider: null,
+    contracts: {},
 
-  init: function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+    init: function() {
+        return App.initWeb3();
+    },
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+    initWeb3: function() {
+        // Is there is an injected web3 instance?
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider;
+        } else {
+            // If no injected web3 instance is detected, fallback to the TestRPC
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+        }
+        web3 = new Web3(App.web3Provider);
+        return App.initContract();
+    },
 
-        petsRow.append(petTemplate.html());
-      }
-    });
+    initContract: function() {
+        $.getJSON('Bounties.json', function(data) {
+            // Get the necessary contract artifact file and instantiate it with truffle-contract
+            var BountyArtifact = data;
+            App.contracts.Bounties = TruffleContract(BountyArtifact);
 
-    return App.initWeb3();
-  },
+            // Set the provider for our contract
+            App.contracts.Bounties.setProvider(App.web3Provider);
 
-  initWeb3: function() {
-    /*
-     * Replace me...
-     */
+            // Use our contract to retrieve and mark the adopted pets
+        });
+        return App.bindEvents();
+    },
 
-    return App.initContract();
-  },
+    bindEvents: function() {
+        //$(document).on('click', '.btn-adopt', App.handleAdopt);
+    },
 
-  initContract: function() {
-    /*
-     * Replace me...
-     */
+    markAdopted: function(adopters, account) {
+        /*
+         * Replace me...
+         */
+    },
 
-    return App.bindEvents();
-  },
+    handleAdopt: function() {
+        event.preventDefault();
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
+        //var petId = parseInt($(event.target).data('id'));
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
-
-  handleAdopt: function() {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
-  }
+        /*
+         * Replace me...
+         */
+    }
 
 };
 
 $(function() {
-  $(window).load(function() {
-    App.init();
-  });
+    $(window).load(function() {
+        App.init();
+    });
 });
+
+function lockFunds() {
+
+}
