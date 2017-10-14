d39c67ffd3eec3345fb41035f90c4a0d90024a1e
Abdulmi <aalmetwa@uwaterloo.ca>
2017-10-14 09:44:18 -0400

    contract name change

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-var Migrations = artifacts.require("./Migrations.sol");
+var Questions = artifacts.require("./questions.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Questions);
 };
