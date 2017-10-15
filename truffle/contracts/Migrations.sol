8ba59b52def69bef61c37c2022226359f428c8c7
Abdulmi <aalmetwa@uwaterloo.ca>
2017-10-14 21:54:17 -0400

    it works

--- truffle/contracts/Migrations.sol
+++ truffle/contracts/Migrations.sol
@@ -1,31 +1,23 @@
 pragma solidity ^0.4.2;
 
-contract questions {
-  mapping(uint => address) public q_to_address;
-  mapping(uint => uint) public q_to_price;
-  mapping(address => uint) public pending_withdrawal;
+contract Migrations {
+  address public owner;
+  uint public last_completed_migration;
 
-
-  modifier restricted(uint question_id) {
-    if (msg.sender == q_to_address[question_id]) _;
+  modifier restricted() {
+    if (msg.sender == owner) _;
   }
 
-  function releaseMilestone(uint question_id, address destination, uint installment) restricted(question_id) {
-    require(msg.value <= q_to_price[question_id]);
-    q_to_price[question_id] -= installment;
-    pending_withdrawal[destination] += installment;
+  function Migrations() {
+    owner = msg.sender;
   }
 
-  function addQuestion(uint question_id) payable {
-    require(q_to_address[question_id] == 0))
-    q_to_address[question_id] = msg.sender;
-    q_to_price[question_id] = msg.value;
+  function setCompleted(uint completed) restricted {
+    last_completed_migration = completed;
   }
 
-  function withdraw() {
-    require(pending_withdrawal[msg.sender] > 0);
-    var amount = pending_withdrawal[msg.sender];
-    pending_withdrawal[msg.sender] = 0;
-    msg.sender.transfer(amount);
+  function upgrade(address new_address) restricted {
+    Migrations upgraded = Migrations(new_address);
+    upgraded.setCompleted(last_completed_migration);
   }
 }
